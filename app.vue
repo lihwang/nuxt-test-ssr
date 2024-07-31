ba4d7ed26110d920fa820fefa5e5808c7db4c99a
lihwang <lihwang_wf@126.com>
2024-07-31 15:13:33 +0800

    提交代码

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    <Card />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import Card from "@/components/Card.vue"
+import { a } from "./index"
+console.log(a);
+
+</script>
---
+++ components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    1111
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps<{
+  ccccs: string
+}>()
+</script>
+
+<style lang="scss" scoped></style>
